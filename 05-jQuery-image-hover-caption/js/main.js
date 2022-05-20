f9b73d567a1fbcaa87f371386a11215ac78510f1
Eunseo Choi <yses9296@gmail.com>
2022-05-21 02:20:02 +0900

    jQuery 05

--- 05-jQuery-image-hover-caption/js/main.js
+++ 05-jQuery-image-hover-caption/js/main.js
@@ -22,8 +22,10 @@ $(function(){
     $images.filter(':nth-child(3)').mouseover(function(){
         $(this).find('span').stop().animate({opacity: 1}, $duration);
         $(this).find('strong').stop().animate({opacity: 1, bottom: '0px'}, $duration);
+        $(this).find('img').stop().animate({top: '-40px'}, $duration);
     }).mouseout(function(){
         $(this).find('span').stop().animate({opacity: 0}, $duration);
         $(this).find('strong').stop().animate({opacity: 0, bottom: '-80px'}, $duration);
+        $(this).find('img').stop().animate({top: '0px'}, $duration);
     })
 });
