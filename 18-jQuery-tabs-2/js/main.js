db8275f647b1d9d5ffc154988f823f3733410084
Eunseo Choi <yses9296@gmail.com>
2022-05-22 17:08:03 +0900

    jQuery 18

--- 18-jQuery-tabs-2/js/main.js
+++ 18-jQuery-tabs-2/js/main.js
@@ -1,19 +1,22 @@
 $(function () {
-	var tabAnchor = $('.tabs-nav  a'),
+	var tabAnchor = $('.tabs-nav li'),
 		tabPanel = $('.tabs-panel');
-		
-	tabAnchor.on('click',function(e){
+	
+	tabAnchor.click(function(e){
 		e.preventDefault();
-		tabAnchor.removeClass('active');
-		$(this).addClass('active');
+
+		$(this).find('a').addClass('active').parent().siblings().find('a').removeClass('active');
 		//var NewLink = $(this).attr('href');
 		
 		tabPanel.hide();
-		$($(this).attr('href')).show();
+
+		var $targetIdx = $(this).index();
+		tabPanel.eq($targetIdx).show();
 		
 	});
 	
-	tabAnchor.eq(0).trigger('click');		
-
+	// .trigger 방아쇠를 당기다.
+	// 창을 열자마자 work01이 나오도록 유도.
+	tabAnchor.eq(0).trigger('click');
 });
 
