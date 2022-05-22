8407400e9202d22367131fac7491a430c06aeeea
Eunseo Choi <yses9296@gmail.com>
2022-05-22 17:21:12 +0900

    jQuery 19

---
+++ 19-jQuery-tabs-3/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+	var tabAnchor = $('.tabs-nav li'),
+		tabPanel = $('.tabs-panel');
+
+	var section = $('.work-section');
+
+	section.tabs({
+		hide: {duration: 200 },
+		show: {duration: 200 }
+	});
+
+});
+
