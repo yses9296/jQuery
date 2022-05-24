2096b5095d0bfbcd460a9529c2dd477b9f374964
Eunseo Choi <yses9296@gmail.com>
2022-05-24 23:10:58 +0900

    jQuery 125

--- 126-jQuery-map-API-2/js/main_3.js
+++ 126-jQuery-map-API-2/js/main_3.js
@@ -1,40 +1,5 @@
 $( function() {
-
-//Tab Menu
-    $( "#tabs" ).tabs({
-        // case 2: jqueryui API
-        create: function( event, ui ) {
-            //ui.panel == $(this)
-            var targetLat = ui.panel.attr('data-lat');
-            var targetLng = ui.panel.attr('data-lng');
-            var targetDiv = ui.panel.find('div').attr('id');
-
-            seoulMap(targetLat, targetLng, targetDiv);
-        },
-        activate: function( event, ui ) {
-            
-            var targetLat = ui.newPanel.attr('data-lat');
-            var targetLng = ui.newPanel.attr('data-lng');
-            var targetDiv = ui.newPanel.find('div').attr('id');
-
-            seoulMap(targetLat, targetLng, targetDiv);
-        }
-    });
-
-    /*
-        case 1: 
-        var tabLink = $('#tabs ul li a');
-
-        tabLink.click(function(e){
-            e.preventDefault();
-            var targetLat = $(this).attr('data-lat');
-            var targetLng = $(this).attr('data-lng');
-            seoulMap(targetLat, targetLng);
-        })//tabLink.click
-    */
-
-
-
+    $( "#accordion" ).accordion();
 //MAP API
     /*
         //초기값 설정
