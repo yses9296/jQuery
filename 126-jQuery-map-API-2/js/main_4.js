5ed351649234ea72adb75d4eae694c865529c4fc
Eunseo Choi <yses9296@gmail.com>
2022-05-24 23:57:57 +0900

    jQuery 126

--- 126-jQuery-map-API-2/js/main_4.js
+++ 126-jQuery-map-API-2/js/main_4.js
@@ -1,5 +1,6 @@
 $( function() {
-//Tab Menu
+
+//CASE 1-1: Tab Menu
     $( "#tabs" ).tabs({
         // case 2: jqueryui API
         create: function( event, ui ) {
@@ -20,7 +21,7 @@ $( function() {
         }
     });//tabs
 
-//MAP API - TAB
+//CASE 1-2: MAP API - TAB
     function seoulMap(lat, lng, field){
         var mapContainer = document.getElementById(field),
         mapOption = { 
@@ -33,7 +34,9 @@ $( function() {
         
     }//seoulMap
     
-//ACCORDION
+
+
+//CASE 2-1: ACCORDION
     $( "#accordion" ).accordion({
         create: function( event, ui ) {
             var targetLat = ui.panel.attr('data-lat');
@@ -49,7 +52,9 @@ $( function() {
         }
     });//accordion
 
-//MAP API - ACCORDION
+
+
+//CASE 2-2:  MAP API - ACCORDION
     function accordionMap(lat, lng, field){
         var mapContainer = document.getElementById(field),
         mapOption = { 
@@ -63,5 +68,63 @@ $( function() {
     }//accordionMap
 
 
+
+//CASE 3-1: MAP LINK
+    function myMap(lat, lng){
+        var mapContainer = document.getElementById('map'),
+        mapOption = { 
+            // center: new kakao.maps.LatLng(location), 
+            center: new kakao.maps.LatLng(lat, lng), 
+            level: 3 
+        };
+    
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+        
+    }//myMap
+
+//CASE 3-2: MENU CLICK
+    $('.maplink li a').click(function(e){
+        e.preventDefault();
+        var targetLat = $(this).attr('data-lat');
+        var targetLng = $(this).attr('data-lng');
+        myMap(targetLat,targetLng);
+    })//maplink click
+
+    //initial location
+    // myMap(37.5727906,126.9769891);
+    $('.maplink li a').eq(0).trigger('click');
+
+
+
+//CASE 4-1: MYTAB
+    $('.mytab ul li a').click(function(e){
+        e.preventDefault();
+        var targetEl = $(this).attr('href');
+
+        // $('.mytab > div').hide();
+        $(targetEl).show();
+
+        var lat = $(targetEl).attr('data-lat');
+        var lng = $(targetEl).attr('data-lng');
+        var targetDiv = $(targetEl).find('div').attr('id');
+        myTapMap(lat, lng, targetDiv);
+
+    })// .mytab ul li a click()
+    //initial location
+    $('.mytab ul li a').eq(0).trigger('click');
+
+
+//CASE 4-2: TAB MENU CLICK
+    function myTapMap(lat, lng, field){
+        var mapContainer = document.getElementById(field),
+        mapOption = { 
+            // center: new kakao.maps.LatLng(location), 
+            center: new kakao.maps.LatLng(lat, lng), 
+            level: 3 
+        };
+    
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+        
+    }//myMap
 
 });
