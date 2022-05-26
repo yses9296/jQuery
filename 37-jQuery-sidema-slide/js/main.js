4459ee289a29003673f099593945da52ca2b93d3
Eunseo Choi <yses9296@gmail.com>
2022-05-26 17:08:23 +0900

    jQuery 37

---
+++ 37-jQuery-sidema-slide/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    new Siema({
+        selector: '.slider',
+        loop: true,
+        // startIndex: 0,
+        // draggable: true,
+        threshold: 20,
+      });
+
+});
