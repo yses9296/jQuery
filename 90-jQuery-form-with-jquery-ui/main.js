ec8f972d50781bd23d31ca833f328fc886c9d157
Eunseo Choi <yses9296@gmail.com>
2022-06-23 23:14:14 +0900

    jQuery 92

--- 90-jQuery-form-with-jquery-ui/main.js
+++ 90-jQuery-form-with-jquery-ui/main.js
@@ -17,14 +17,20 @@ $( function() {
         "SQL"
     ];
     $( "#category" ).autocomplete({
-        source: availableTags
+        source: availableTags,
+        change: function( event, ui ) {
+            updateProgress();
+        }
     });
 
     //spinner
     $( "#experience" ).spinner({
         min: 0,
         max: 10,
-        step: 2
+        step: 1,
+        change: function( event, ui ) {
+            updateProgress();
+        }
     });
     
     // dataPicker
@@ -44,4 +50,40 @@ $( function() {
     $( "#submit" ).click( function( e ) {
       e.preventDefault();
     } );
+
+    //progressBar
+
+    $("#progress").progressbar({
+        value: 0
+    });
+    $('input').change(function(){
+        updateProgress();
+    })
+    function updateProgress(){
+        var $progress = 0,
+            $itemCount = 5,
+            $itemCompleted = 0;
+        var $location = $('#location option:selected').val();
+        var $category = $('#category').val();
+        var $expreience = $('#experience').val();
+        var $date = $('#startDate').val();
+        var $refer = $("input[type='radio']:checked").val();
+
+        if ($location) $itemCompleted++;
+        if ($category) $itemCompleted++;
+        if ($expreience) $itemCompleted++;
+        if ($date) $itemCompleted++;
+        if ($refer) $itemCompleted++;
+
+        $progress = $itemCompleted/$itemCount * 100;
+        $( "#progress" ).progressbar( "option", "value", $progress );
+        $( ".percent span" ).text($progress);
+
+        if($progress == 100)
+        $( "#submit" ).button({
+            disabled: false
+        });
+
+    }//updateProgress
+    updateProgress();
 });//document ready
