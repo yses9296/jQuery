31b24972e79bafb6c386677d521bdaf5898bfaa3
Eunseo Choi <yses9296@gmail.com>
2022-06-25 15:12:21 +0900

    jQuery 93

---
+++ 93-jQuery-star-rating/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    var rating = $('.review .rating');
+
+    rating.each(function(){
+        var targetScore = $(this).attr('data-rate');
+        $(this).find('svg:nth-child(-n+'+ targetScore+')').css({color: '#F05522 '});
+    });//rating each
+
+
+    var userScore = $('#makeStar');
+    var target = $('.make_star .rating');
+
+    userScore.change(function(){
+        var userScoreNum = $(this).val();
+        target.find('svg').css({color: '#000'})
+        target.find('svg:nth-child(-n+'+ userScoreNum+')').css({color: '#F05522'});
+    
+    })//userScroe change
+
+})
