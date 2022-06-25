890c72c7c1fa5126145c32ce1215d4f43113a7b7
Eunseo Choi <yses9296@gmail.com>
2022-06-25 15:14:42 +0900

    jQuery 93

--- 93-jQuery-star-rating/main.js
+++ 93-jQuery-star-rating/main.js
@@ -13,9 +13,15 @@ $(function(){
 
     userScore.change(function(){
         var userScoreNum = $(this).val();
-        target.find('svg').css({color: '#000'})
+        target.find('svg').css({color: '#000'});
         target.find('svg:nth-child(-n+'+ userScoreNum+')').css({color: '#F05522'});
-    
     })//userScroe change
+
+    target.find('svg').click(function(){
+        var targetNum = $(this).index() + 1;
+        target.find('svg').css({color: '#000'});
+        target.find('svg:nth-child(-n+'+ targetNum+')').css({color: '#F05522'});
+        userScore.val(targetNum);
+    })//target svg click
 
 })
