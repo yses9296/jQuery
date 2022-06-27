2d3576b923964322962c3760ec2d7f4a90759b66
Eunseo Choi <yses9296@gmail.com>
2022-06-27 23:25:49 +0900

    jQuery 96

--- 96-jQuery-parallax-01/js/main.js
+++ 96-jQuery-parallax-01/js/main.js
@@ -1,5 +1,30 @@
 $(function(){
-    
+    var $section = $('.parallax > div'),
+        $sectionInfo = [];
 
+    $section.each(function(){
+        var $this = $(this);
+        $sectionInfo.push($this.offset().top);
+    });
+
+    $section.css({position: 'fixed'})
+
+    $(window).scroll(function(){
+        var sct = $(this).scrollTop();
+
+        $section.each(function(index){
+            var $this = $(this);
+            var $newTop = $sectionInfo[index] - sct;
+
+            if(sct > $sectionInfo[index]){
+                $newTop *= 0.5;
+            }
+
+            $this.css({top: $newTop});
+        })
+
+        
+
+    })//window scroll event
 
 })//document ready
