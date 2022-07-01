b0bf75bf4f1c6d74b08b7a21a8d1330178118c77
Eunseo Choi <yses9296@gmail.com>
2022-07-01 23:39:47 +0900

    jQuery 97

--- 97-jQuery-parallax-02/js/main.js
+++ 97-jQuery-parallax-02/js/main.js
@@ -4,25 +4,37 @@ $(function(){
 
     $section.each(function(i){
         var $this = $(this);
-        var $upBtn = $this.find('.arrow > a:eq(0)');
-        var $downBtn = $this.find('.arrow > a:eq(1)');
+        var $upBtn = $this.find('.arrow .upBtn');
+        var $downBtn = $this.find('.arrow .downBtn');
 
         $sectionInfo.push($this.offset().top);
 
         $upBtn.click(function(e){
             e.preventDefault();
-            move(i-1);
+            if(i == 0) {
+                return;
+            }
+            else {
+                move(i-1);
+            }
         });
         $downBtn.click(function(e){
             e.preventDefault();
-            move(i+1);
+            if (i == $sectionInfo.length - 1){
+                return;
+            }
+            else {
+                move(i+1);
+            }
         });
 
     });//section each
 
     function move(index){
         var scr = $sectionInfo[index];
-        $('html, body').stop().animate({scrollTop: scr}, 600, 'easeOutCubic')
+        console.log(scr);
+        $('html, body').stop().animate({scrollTop: scr}, 600, 'easeOutCubic');
+        console.log("Scrolling Succedd")
     }
 
     $section.css({position: 'fixed'})
