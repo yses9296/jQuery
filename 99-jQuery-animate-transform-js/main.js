dca73498d16938bc92e059edcc15206ca5b1490f
Eunseo Choi <yses9296@gmail.com>
2022-07-04 21:30:14 +0900

    jQuery 99

---
+++ 99-jQuery-animate-transform-js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('button').click(function(){
+        // $('div').animate({left: '250px'});
+        // $('div').animate({marginLeft: '250px'});
+        $('div').animate({transform: 'translateX(250px) rotate(90deg) scale(1.5) '});
+    })
+
+
+})//document ready
