fa20b75d152d7a2a2c14ea6286dbfe69e309f23c
Eunseo Choi <yses9296@gmail.com>
2022-08-23 20:26:55 +0900

    jQuery 129

--- 129-jQuery-slick-js/main.js
+++ 129-jQuery-slick-js/main.js
@@ -2,10 +2,18 @@ $(function(){
     $('.category_list').slick({
         dots: false,
         arrows: false,
-        infinite: false,
+        infinite: true,
         speed: 300,
         slidesToShow: 1,
         centerMode: false,
         variableWidth: true,
       });
+
+    let categoryList = $('.category_list li');
+
+    $('.category_list').on('afterChange', function(event, slick, currentSlide, nextSlide){
+      let targetURL = categoryList.eq(currentSlide).find('a').attr('href');
+      location.href = targetURL;
+    });
+             
 })
